d0a3c80ed4f6b1eb655690486d501242480eeee8
Peter Doyle <peter@Peters-iMac.hsd1.co.comcast.net>
2016-07-06 05:12:42 -0600

    added couchbase support

---
+++ provision/couchbase.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+package='couchbase-server-community-4'
+eval yum list installed "$package" >/dev/null 2>&1
+if [ $? -eq 1 ]; then
+  curl -O -L http://packages.couchbase.com/releases/4.0.0/couchbase-server-community-4.0.0-centos7.x86_64.rpm \
+  && yum localinstall couchbase-server-community-4.0.0-centos7.x86_64.rpm \
+  && rm -f couchbase-server-community-4.0.0-centos7.x86_64.rpm
+else
+  echo -e "\e[7;44;96m*scouchbase-server-community-4.0.0 already appears to be installed. skipping."
+fi
