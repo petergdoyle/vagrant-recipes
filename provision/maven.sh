9d2c013b78f2be8f758e16be4db8a5022156a106
Peter Doyle <peter.g.doyle@gmail.com>
2016-07-12 15:05:47 -0600

    update maven.sh
    changed the version of the download to the latest release

--- provision/maven.sh
+++ provision/maven.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-eval 'mvn -version' > /dev/null 2>&1
-if [ $? -eq 127 ]; then
-  mkdir /usr/maven
-  #install maven
-  curl -O http://www.eu.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz \
-    && tar -xvf apache-maven-3.3.3-bin.tar.gz -C /usr/maven \
-    && ln -s /usr/maven/apache-maven-3.3.3 /usr/maven/default \
-    && rm -f apache-maven-3.3.3-bin.tar.gz
+  eval 'mvn -version' > /dev/null 2>&1
+  if [ $? -eq 127 ]; then
+    mkdir /usr/maven
+    #install maven
+    curl -O http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz \
+      && tar -xvf apache-maven-3.3.9-bin.tar.gz -C /usr/maven \
+      && ln -s /usr/maven/apache-maven-3.3.9 /usr/maven/default \
+      && rm -f apache-maven-3.3.9-bin.tar.gz
 
-  alternatives --install "/usr/bin/mvn" "mvn" "/usr/maven/default/bin/mvn" 99999
+    alternatives --install "/usr/bin/mvn" "mvn" "/usr/maven/default/bin/mvn" 99999
 
-  export MAVEN_HOME=/usr/maven/default
-  cat >/etc/profile.d/maven.sh <<-EOF
+    export MAVEN_HOME=/usr/maven/default
+    cat >/etc/profile.d/maven.sh <<-EOF
 export MAVEN_HOME=$MAVEN_HOME
 EOF
 
-else
-  echo -e "\e[7;44;96m*maven already appears to be installed. skipping."
-fi
+  else
+    echo -e "\e[7;44;96mmaven already appears to be installed. skipping."
+  fi
